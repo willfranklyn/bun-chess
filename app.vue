02c0b6d1003d0611d26dd7c585fdce08dff78098
willfranklyn <willfranklyn2@gmail.com>
2023-09-30 15:15:13 -0700

    Create Basic Chessboard Component

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Chessboard />
   </div>
 </template>
---
+++ components/Chessboard.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Chessboard goes here!</h1>
+    </div>
+</template>
